9dd96cf9a6594bd947b27276c794bb28b67d4ab3
Mithesh <mithesh109@gmail.com>
2021-02-04 18:45:28 +0530

    Logging out Users

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "/about", to: 'about#index'
   get "/sign_up", to: 'registrations#new'
   post "/sign_up", to: 'registrations#create'
+  delete "/logout", to: 'sessions#destroy'
 end
